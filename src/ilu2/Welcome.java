2583e5316ded4b3ef00dae9f4a61ff3edd49e26c
JULIEN bonnafe <julien.bonnafe1@univ-tlse3.fr>
2023-05-05 22:54:13 +0200

    EX_7

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -1,5 +1,7 @@
 package ilu2;
 
+import java.util.*;
+
 public class Welcome {
 	
 	public static String welcome(String input) {
@@ -8,8 +10,8 @@ public class Welcome {
 		if(input==""){return "Hello,my friend";}
 		if (MajEquals(input)){return("Hello,"+input+"!");}
 		String[] word=input.split(",");
-		if (word.length==2) {return ("Hello,"+Maj(word[0])+","+Maj(word[1]));}
-		if (word.length>2) {return("Hello"+StrNom(word));}
+		if (word.length==2) {return ("Hello,"+Maj(word[0])+" and "+Maj(word[1]));}
+		if (word.length>2) {return(StrNom(word));}
 		return("Hello,"+Maj(input));
 	}
 	public static String Maj(String input) {
@@ -18,15 +20,33 @@ public class Welcome {
 	public static String StrNom(String[] word) {
 		String input="";
 		String Majinput="";
+		List<String> wordMinuscule=TrieMin(word),wordMajuscule=TrieMaj(word);
+		if (wordMajuscule.size()>0) {Majinput=".AND HELLO";}
+		for ( int i=0;i<wordMajuscule.size();i++) {
+			if ((i==wordMajuscule.size()-1)&&(wordMajuscule.size()>1)) {Majinput=Majinput+" AND "+wordMajuscule.get(i)+"!";}
+			else{Majinput=Majinput+","+wordMajuscule.get(i);}}
+		for (int i=0;i<wordMinuscule.size();i++) {
+			if ((i==wordMinuscule.size()-1)&&(wordMinuscule.size()-1>0)) {input=input+" and "+Maj(wordMinuscule.get(i));}
+			else {input=input+","+Maj(wordMinuscule.get(i));}}
+		if (input==""){return "HELLO,"+Majinput;}
+		if(wordMajuscule.size()==1) {Majinput=Majinput+"!";}
+		return "Hello"+input+Majinput;
+	}
+	public static List<String> TrieMaj(String[] word) {
+		List<String> wordMajuscule= new LinkedList<String>();
 		for (int i=0;i<word.length;i++) {
-			if (MajEquals(word[i])) {
-				Majinput=Majinput+".AND HELLO,"+word[i]+"!";}
-			else {input=input+","+Maj(word[i]);}
-				}
-			
-		return input+Majinput;
+			if (MajEquals(word[i])) {wordMajuscule.add(word[i]);}
+		}
+		return  (wordMajuscule) ; 
 	}
 	public static Boolean MajEquals(String input) {
-		return ((input.toUpperCase().equals(input)));
-	} 
+		return ((input.toUpperCase().equals(input)));} 
+	
+	public static List<String> TrieMin(String[] word) {
+		List<String> wordMinuscule = new LinkedList<String> ();
+		for (int i=0;i<word.length;i++) {
+			if (!MajEquals(word[i])) {wordMinuscule.add(word[i]);}}
+		return( wordMinuscule);
+		}
+
 }
--- src/ilu2/testW/WelcomeTest1.java
+++ src/ilu2/testW/WelcomeTest1.java
@@ -32,17 +32,23 @@ class WelcomeTest1 {
 	@Test
 	void welcometest4() {
 		String nom="amy,bob";
-		assertEquals(Welcome.welcome(nom),"Hello,Amy,Bob");
+		assertEquals(Welcome.welcome(nom),"Hello,Amy and Bob");
 	}
 	@Test
 	void welcometest5() {
 		String nom="amy,bob,jerry";
-		assertEquals(Welcome.welcome(nom),"Hello,Amy,Bob,Jerry");
+		assertEquals(Welcome.welcome(nom),"Hello,Amy,Bob and Jerry");
 	}
 	@Test
 	void welcometest6() {
 		String nom="Amy,BOB,Jerry";
-		assertEquals(Welcome.welcome(nom),"Hello,Amy,Jerry.AND HELLO,BOB!");
+		assertEquals(Welcome.welcome(nom),"Hello,Amy and Jerry.AND HELLO,BOB!");
 	}
-	}
+	@Test
+	void welcometest7() {
+		String nom="bob,AMY,jerry,JACK";
+		assertEquals(Welcome.welcome(nom),"Hello,Bob and Jerry.AND HELLO,AMY AND JACK!");
+		nom="bob,amy,jerry";
+		assertEquals(Welcome.welcome(nom),"Hello,Bob,Amy and Jerry");
+	}}
 
